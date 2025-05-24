985478c0dbd4d930565dedfd61b956762dd0b85d
shivampandey2389 <shivampandey2389@gmail.com>
2025-05-24 23:45:53 +0530

    make changes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,42 +1,37 @@
 import express from "express";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-
-import path from "path";
-
-import { connectdb } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import dotenv from "dotenv";
+import { connectdb } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import cors from"cors";
 import { app, server } from "./lib/socket.js";
+import path from "path"
 
-dotenv.config();
+dotenv.config()
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+app.use(cors(
+  {
+    origin:"http://localhost:5173",
+    credentials:true,
+  }
+))
+app.use('/api/auth',authRoutes);
+app.use("/api/messages",messageRoutes);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../Frontend/dist")))
+  app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../Frontend","dist","index.html"));
   })
-);
-
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
-  });
 }
 
-server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
-  connectdb();
-});
+server.listen(PORT,()=>{
+  console.log(`server is running at http://localhost:5001`);
+  connectdb()
+})
